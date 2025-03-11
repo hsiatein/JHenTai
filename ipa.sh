f01b9dc96965a1e02d564a1c3c5471d09f259f07
XiaTian <3023625451@qq.com>
2025-03-11 11:19:45 +0800

    1

--- ipa.sh
+++ ipa.sh
@@ -1,8 +1,10 @@
-version=$(head -n 5 pubspec.yaml | tail -n 1 | cut -d ' ' -f 2)
-
-flutter build ios --release -t lib/src/main.dart \
-&& mkdir ~/Desktop/Payload \
-&& cp -r build/ios/Release-iphoneos/Runner.app/ ~/Desktop/Payload/Runner.app/ \
-&& cd ~/Desktop \
-&& zip -ro JHenTai_${version}.ipa Payload \
-&& rm -rf Payload
+flutter precache --ios
+cd ios
+pod update
+cd ..
+flutter build ios -t lib/src/main.dart --release --no-codesign
+sh thin-payload.sh build/ios/iphoneos/*.app
+cd build
+mkdir -p Payload
+mv ios/iphoneos/*.app Payload
+zip -9 JHenTai_8.0.6+278.ipa -r Payload
